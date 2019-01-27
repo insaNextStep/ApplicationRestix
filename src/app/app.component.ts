72561836f6f20cd9f6a0c82a62599d8eb5c9211a
ajchahid <abdeljalile.chahid@gmail.com>
2019-01-27 18:27:21 +0100

    Introduction à Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
 import { Component } from '@angular/core';
 
+/*
+selector  : il s'agit du nom qu'on utilisera comme balise
+HTML pour afficher ce component, comme vous l'avez vu avec
+<app-root>  . Ce nom doit être unique et ne doit pas être
+un nom réservé HTML de type  <div>  ,  <body>  etc. On utilisera
+donc très souvent un préfixe comme  app  , par exemple ;
+templateUrl  : le chemin vers le code HTML à injecter ;
+styleUrls  : un array contenant un ou plusieurs chemins vers les feuilles de styles qui concernent ce component ;
+*/
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+// exportation des élements
 export class AppComponent {
-  title = 'TutoAngular';
+  constructor() {
+    // méthode constuction d'un timer
+    setTimeout(() => {
+      this.isAuth = true;
+    }, this.msTemps);
+  }
+  isAuth = false; // autentification par défaut
+  totalTransaction: Number = 0;
+  // tableau de valeur pouvant provenir de l'extérieur
+  transactions = [
+    {
+      name: 1234,
+      value: 20
+    },
+    {
+      name: 2345,
+      value: 10
+    },
+    {
+      name: 3456,
+      value: -5
+    }
+  ];
+
+  msTemps = 4000; // timer pour activer bouton
+
+  // Sommes des transactions
+  totalTrans() {
+    let newLocal = 0;
+    for (let i = 0; i < this.transactions.length; i++) {
+      newLocal = newLocal + this.transactions[i].value;
+    }
+    this.totalTransaction = newLocal;
+    return this.totalTransaction;
+  }
+
+  onAllumer() {
+    console.log('On allume tout !');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatMenuModule} from '@angular/material/menu';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TransactionComponent } from './transaction/transaction.component';
+/*
+Pour pouvoir utiliser le two-way binding,
+il vous faut importer  FormsModule  depuis
+@angular/forms  dans votre application.
+*/
+import { FormsModule } from '@angular/forms';
+import { BarreMenuComponent } from './barre-menu/barre-menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransactionComponent,
+    BarreMenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    MatMenuModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
