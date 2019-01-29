454384c8aa794010f580918b0d064f4026c5cb38
ajchahid <abdeljalile.chahid@gmail.com>
2019-01-29 12:21:09 +0100

    ajout du composant barre menu

---
+++ src/app/barre-menu/barre-menu.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-barre-menu',
+  templateUrl: './barre-menu.component.html',
+  styleUrls: ['./barre-menu.component.scss']
+})
+export class BarreMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+export class AppBarreMenu {}
