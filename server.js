34a498460b10ea452f28c593e4ee95dfe5dbde31
Abdel Jalile CHAHID <abdeljalile.chahid@gmail.com>
2019-03-13 23:34:34 +0100

    heroku3

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/restix-api'));
+app.use(express.static(__dirname + '/dist/ApplicationRestix'));
 
 // Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/restix-api/index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/ApplicationRestix/index.html'));
 });
 
 // default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, ()=> {
+  console.log('lancement du server');
+});
