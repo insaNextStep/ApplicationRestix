e0a382bd63f1b93a20d76ddf19f1fe248a6c0201
Abdel Jalile CHAHID <abdeljalile.chahid@gmail.com>
2019-03-13 22:31:11 +0100

    commit pour heroku

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/restix'));
+app.use(express.static(__dirname + '/dist/restix-api'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/restix/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/restix-api/index.html'));
 });
 
 // default Heroku port
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 8080);
